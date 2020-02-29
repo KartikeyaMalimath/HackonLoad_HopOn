344bbe8c004cb3c6e891a14e37ace5e5d89df359
KartikeyaMalimath <kartikeyapm@gmail.com>
2020-02-29 21:36:27 +0530

    customer login flow

--- js/login.js
+++ js/login.js
@@ -7,7 +7,9 @@
             // User successfully signed in.
             // Return type determines whether we continue the redirect automatically
             // or whether we leave that to developer to handle.
-            return true;
+            var user = authResult.user;
+            console.log(user.email);
+            return authResult;
           },
           uiShown: function() {
             // The widget is rendered.
@@ -17,7 +19,7 @@
         },
         // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
         signInFlow: 'popup',
-        signInSuccessUrl: './test.html',
+        signInSuccessUrl: './public/index.html',
         signInOptions: [
           // Leave the lines as is for the providers you want to offer your users.
           firebase.auth.GoogleAuthProvider.PROVIDER_ID,
@@ -28,9 +30,9 @@
           // firebase.auth.PhoneAuthProvider.PROVIDER_ID
         ],
         // Terms of service url.
-        // tosUrl: './test.html',
-        // // Privacy policy url.
-        // privacyPolicyUrl: './test.html'
+        tosUrl: './test.html',
+        // Privacy policy url.
+        privacyPolicyUrl: './test.html'
       };
 
       ui.start('#firebaseui-auth-container', uiConfig); 
